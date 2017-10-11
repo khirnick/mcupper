b75e11acd296571226fe6f8c6eecbf5c76f515c9
hitryy <khirnick@yandex.ru>
2017-10-11 17:06:47 +0300

    add user entering in room

--- cupper/consumers.py
+++ cupper/consumers.py
@@ -12,7 +12,6 @@ def ws_connect(message):
 
 def ws_receive(message):
     a = json.loads(message['text'])
-    Group('cupper').send({'text': json.dumps({'user_id': a['user_id']})})
     payload = a
     payload['reply_channel'] = message.content['reply_channel']
     Channel("cupper.receive").send(payload)
@@ -23,5 +22,21 @@ def ws_disconnect(message):
 
 
 def room_join(message):
-    print(message['room'], " ", message.content)
-    message.reply_channel.send({'text': json.dumps({'user_id': message.content['user_id']})})
+    user_id = message.content['user_id']
+    room_id = int(message.content['room'])
+    room = GameMain.get_room_by_id(room_id)
+
+    print(room.user_channels)
+
+    user_channel = room.user_channels.get(user_id)
+    if user_channel is None:
+        room.user_channels[user_id] = message.reply_channel
+
+        all_users = ''
+        for u in room.user_channels:
+            all_users += ("{0}\n".format(u))
+
+        print(all_users)
+
+        for u in room.user_channels:
+            room.user_channels[u].send({'text': json.dumps({'user_id': all_users})})
--- cupper/urls.py
+++ cupper/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     url(r'activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'),
     url(r'activation_success/$', views.activation_success),
-    url(r'game/', views.game),
+    url(r'game/', views.game, name='game'),
     url(r'room/(?P<room_id>[0-9]+)/$', views.room, name='room')
 
 ]
