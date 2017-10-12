4f32431b1c6e2a28d030d20e6b3908380bbba3ad
hitryy <khirnick@yandex.ru>
2017-10-12 21:51:37 +0300

    huge refactoring, move some logic to model

--- cupper/consumers.py
+++ cupper/consumers.py
@@ -10,36 +10,16 @@ def ws_connect(message):
 
 
 def ws_receive(message):
-    a = json.loads(message['text'])
-    payload = a
-    payload['reply_channel'] = message.content['reply_channel']
-    Channel("cupper.receive").send(payload)
+    message_text = json.loads(message['text'])
+    message_to_another_channel = message_text
+    message_to_another_channel['reply_channel'] = message.content['reply_channel']
+
+    Channel("cupper.receive").send(message_to_another_channel)
 
 
 def ws_disconnect(message):
-    print(message.reply_channel)
-
-    key_to_delete = []
-
-    for room in GameMain.rooms:
-        for u in room.user_channels:
-            print(room.user_channels[u])
-            if str(message.reply_channel) == str(room.user_channels[u]):
-                key_to_delete = u
-                break
-        room.user_channels.pop(key_to_delete, None)
-
-    for room in GameMain.rooms:
-        for u in room.user_channels:
-            print(u)
-
-    all_users = ''
-    for room in GameMain.rooms:
-        for u in room.user_channels:
-            all_users += ("{0}\n".format(u))
-        for u in room.user_channels:
-            room.user_channels[u].send({'text': json.dumps({'user_id': all_users})})
-        all_users = ''
+    GameMain.try_to_delete_user_from_rooms(message.reply_channel)
+    GameMain.update_user_group()
 
 
 def room_join(message):
@@ -47,27 +27,13 @@ def room_join(message):
     room_id = int(message.content['room'])
     room = GameMain.get_room_by_id(room_id)
 
-    if len(room.user_channels) == room.max_channels:
-        return
-
-    user_channel = room.add_user_channel(user_id, message.reply_channel)
-
-    if user_channel:
-        all_users = ''
-        for u in room.user_channels:
-            all_users += ("{0}\n".format(u))
-
-        room.send_to_all_users_over_websocket({'user_id': all_users})
+    if room.add_user_channel(user_id, message.reply_channel):
+        room.update_user_group()
 
     if not room.game_is_online:
-        if len(room.user_channels) == room.max_channels:
-            room.game_online = True
-            task = room.update_current_task()
-            room.current_task_no += 1
-            for u in room.user_channels:
-                room.user_scores[u] = 0
-            for u in room.user_channels:
-                room.user_channels[u].send({'text': json.dumps({'game_start': True, 'image': str(task.image)})})
+        if room.is_busy:
+            room.start_game()
+            room.continue_game_with_next_question()
 
 
 def room_leave(message):
@@ -80,14 +46,7 @@ def room_leave(message):
         ch.send({'text': json.dumps({'user_leave': True})})
         del room.user_channels[int(user_id)]
 
-    all_users = ''
-    for u in room.user_channels:
-        if room.user_channels[u] is not None:
-            all_users += ("{0}\n".format(u))
-
-    for u in room.user_channels:
-        if room.user_channels[u] is not None:
-            room.user_channels[u].send({'text': json.dumps({'user_id': all_users})})
+    room.update_user_group()
 
 
 def result(message):
@@ -97,11 +56,4 @@ def result(message):
     user_id = int(message.content['user_id'])
 
     room.check_answer(user_id, answer)
-
-    if room.current_task_no == room.task_limit:
-        user_id_winner = room.get_max_score()
-        room.send_to_user_over_websocket_by_id(user_id_winner, {'ifwinner': True})
-        room.reset_room()
-    else:
-        task = room.update_current_task()
-        room.send_to_all_users_over_websocket({'game_start': True, 'image': str(task.image)})
+    room.continue_game_with_next_question()
--- cupper/room.py
+++ cupper/room.py
@@ -19,6 +19,14 @@ class Game:
     def get_room_by_id(self, id):
         return self.rooms[id]
 
+    def try_to_delete_user_from_rooms(self, channel):
+        for room in self.rooms:
+            room.try_to_delete_disconnected_user_from_room(channel)
+
+    def update_user_group(self):
+        for room in self.rooms:
+            room.update_user_group()
+
 
 class Room:
     def __init__(self, id):
@@ -38,10 +46,10 @@ class Room:
     @property
     def is_busy(self):
         if len(self.user_channels) == self.max_channels:
-            return False
+            return True
 
     def add_user_channel(self, user_id, user_channel):
-        if len(self.user_channels) >= self.max_channels:
+        if self.is_busy:
             return False
 
         user = self.user_channels.get(user_id)
@@ -61,7 +69,7 @@ class Room:
         if user_channel is not None:
             user_channel.send({'text': json.dumps(data)})
 
-    def get_max_score(self):
+    def get_user_id_of_winner(self):
         return max(self.user_scores, key=self.user_scores.get)
 
     def check_answer(self, user_id_answered, answer):
@@ -70,13 +78,46 @@ class Room:
         else:
             self.user_scores[user_id_answered] += 1
 
-        self.current_task_no += 1
-
     def reset_room(self):
         self.game_is_online = False
         self.user_channels.clear()
         self.user_scores.clear()
         self.current_task_no = 0
+
+    def try_to_delete_disconnected_user_from_room(self, channel):
+        key_to_delete = None
+        for user_id, user_channel in self.user_channels.items():
+            if str(channel) == str(user_channel):
+                self.user_channels.pop(user_id, None)
+                break
+
+    def update_user_group(self):
+        user_group = ''
+        for user_id, channel in self.user_channels.items():
+            user_group += "{0}\n".format(user_id)
+
+        self.send_to_all_users_over_websocket({'user_id': user_group})
+
+    def start_game(self):
+        for user_id, channel in self.user_channels.items():
+            self.user_scores[user_id] = 0
+
+        self.game_is_online = True
+
+    def continue_game_with_next_question(self):
+        if self.current_task_no == self.task_limit:
+            user_id_winner = self.get_user_id_of_winner()
+            self.send_to_user_over_websocket_by_id(user_id_winner, {'ifwinner': True})
+            for user_id, channel in self.user_channels.items():
+                if user_id != user_id_winner:
+                    print(user_id)
+                    self.send_to_user_over_websocket_by_id(user_id, {'ifloser': True})
+            self.reset_room()
+        else:
+            task = self.update_current_task()
+            self.send_to_all_users_over_websocket({'game_start': True, 'image': str(task.image)})
+            self.current_task_no += 1
+
 
 
 
